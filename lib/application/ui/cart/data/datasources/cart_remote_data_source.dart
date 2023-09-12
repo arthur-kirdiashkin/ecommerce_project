9b33fd221883cd0043b4c2a0154f409d36a7d8f8
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-12 20:14:13 +0300

    worked with cart data response

--- lib/application/ui/cart/data/datasources/cart_remote_data_source.dart
+++ lib/application/ui/cart/data/datasources/cart_remote_data_source.dart
@@ -2,40 +2,55 @@ import 'dart:convert';
 
 import 'package:ecommerce_project/application/ui/cart/data/models/basket_model.dart';
 import 'package:ecommerce_project/application/ui/cart/data/models/cart_model.dart';
+import 'package:ecommerce_project/application/ui/core/error/exception.dart';
 
 import 'package:http/http.dart' as http;
 
-class ApiClientCart {
-  Future<List<CartModel>> getCart() async {
-    final urlCart = Uri.parse(
-        'https://run.mocky.io/v3/53539a72-3c5f-4f30-bbb1-6ca10d42c149');
-    final response = await http.get(urlCart);
-    if (response.statusCode == 200) {
-      List<CartModel> cartModelList = [];
-      final cartJson = json.decode(response.body);
-      CartModel results = CartModel.fromJson(cartJson);
-      cartModelList.add(results);
-      return cartModelList;
-      // return (cartJson as List<dynamic>)
-      //     .map((e) => CartModel.fromJson(e))
-      //     .toList();
-    } else {
-      throw Exception('Error getting Api Cart');
-    }
-  }
-}
-//   Future<List<BasketModel>> getBasket() async {
-//     final urlBasket = Uri.parse(
+// class ApiClientCart {
+//   Future<List<CartModel>> getCart() async {
+//     final urlCart = Uri.parse(
 //         'https://run.mocky.io/v3/53539a72-3c5f-4f30-bbb1-6ca10d42c149');
-//     final response = await http.get(urlBasket);
-//     if (response.hashCode == 200) {
-//       // List<CartModel> cartModelList = [];
-//       final basketJson = json.decode(response.body);
-//       return (basketJson['basket'] as List<dynamic>)
-//           .map((e) => BasketModel.fromJson(e))
-//           .toList();
+//     final response = await http.get(urlCart);
+//     if (response.statusCode == 200) {
+//       List<CartModel> cartModelList = [];
+//       final cartJson = json.decode(response.body);
+//       CartModel results = CartModel.fromJson(cartJson);
+//       cartModelList.add(results);
+//       return cartModelList;
 //     } else {
-//       throw Exception('Error getting Api Basket');
+//       throw Exception('Error getting Api Cart');
 //     }
 //   }
 // }
+
+abstract class CartRemoteDataSource {
+  Future<List<CartModel>> getAllCarts();
+  Future<List<CartModel>> getBasket();
+}
+
+class CartRemoteDataSourceImpl implements CartRemoteDataSource {
+  final http.Client client;
+
+  CartRemoteDataSourceImpl({required this.client});
+  @override
+  Future<List<CartModel>> getAllCarts() => _getCartsFromUrl(
+      'https://run.mocky.io/v3/53539a72-3c5f-4f30-bbb1-6ca10d42c149');
+
+  @override
+  Future<List<CartModel>> getBasket() => _getCartsFromUrl(
+      'https://run.mocky.io/v3/53539a72-3c5f-4f30-bbb1-6ca10d42c149');
+
+  Future<List<CartModel>> _getCartsFromUrl(String url) async {
+    final urlCart = Uri.parse(url);
+    final response = await client.get(urlCart);
+    if (response.statusCode == 200) {
+      List<CartModel> cartList = [];
+      final cartJson = jsonDecode(response.body);
+      final results = CartModel.fromJson(cartJson);
+      cartList.add(results);
+      return cartList;
+    } else {
+      throw ServerException();
+    }
+  }
+}
