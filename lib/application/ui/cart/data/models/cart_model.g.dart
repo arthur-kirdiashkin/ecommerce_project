cfd3347fb64bc9d49f4360a0c7c8009b41a01166
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-12 20:15:16 +0300

    refactored cart model

--- lib/application/ui/cart/data/models/cart_model.g.dart
+++ lib/application/ui/cart/data/models/cart_model.g.dart
@@ -7,12 +7,12 @@ part of 'cart_model.dart';
 // **************************************************************************
 
 CartModel _$CartModelFromJson(Map<String, dynamic> json) => CartModel(
-      basket: (json['basket'] as List<dynamic>)
-          .map((e) => BasketModel.fromJson(e as Map<String, dynamic>))
+      basket: (json['basket'] as List<dynamic>?)
+          ?.map((e) => BasketModel.fromJson(e as Map<String, dynamic>))
           .toList(),
-      delivery: json['delivery'] as String,
-      id: json['id'] as String,
-      total: json['total'] as int,
+      delivery: json['delivery'] as String?,
+      id: json['id'] as String?,
+      total: json['total'] as int?,
     );
 
 Map<String, dynamic> _$CartModelToJson(CartModel instance) => <String, dynamic>{
