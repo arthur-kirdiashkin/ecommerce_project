c21e56f2b1b5eef375236b5c3d0a127aa28c9271
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-16 09:14:11 +0300

    renamed the folder

--- lib/application/ui/core/usecases/usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:ecommerce_project/application/ui/core/error/failure.dart';
-
-abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
-
-}
