7e854459504b615d677bc0dcf44b093a80860ca9
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-16 09:46:14 +0300

    ecommerce_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
-import 'package:ecommerce_project/application/ui/cart/presentation/bloc/cart_bloc.dart';
-import 'package:ecommerce_project/application/ui/themes/app_theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'application/ui/cart/domain/repositories/cart_repository.dart';
-import 'application/ui/cart/presentation/bloc/cart_event.dart';
-import 'application/ui/home/domain/repositories/home_repository.dart';
-import 'application/ui/home/presentation/bloc/phone_bloc.dart';
-import 'application/ui/home/presentation/home_widgets/home_bottom_bar_widget.dart';
 import 'package:ecommerce_project/locator_service.dart' as di;
+import 'features/cart/presentation/bloc/cart_bloc.dart';
+import 'features/cart/presentation/bloc/cart_event.dart';
+import 'features/home/presentation/bloc/home_bloc.dart';
+import 'features/home/presentation/home_widgets/home_bottom_bar_widget.dart';
+import 'features/product/presentation/bloc/product_bloc.dart';
+import 'features/product/presentation/bloc/product_event.dart';
+import 'features/themes/app_theme.dart';
+import 'locator_service.dart';
 
-import 'application/ui/product/domain/repositories/one_phone_repository.dart';
-import 'application/ui/product/presentation/bloc/product_bloc.dart';
-import 'application/ui/product/presentation/bloc/product_event.dart';
-// C:\vovan_arthur\ecommerce_project\ecommerce_project\lib\locator_service.dart;
-
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await di.init();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -25,21 +22,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _cartRepository = CartRepository();
-    final _onePhoneRepository = OnePhoneRepository();
-    final _phoneRepository = PhoneRepository();
     return MultiBlocProvider(
       providers: [
         BlocProvider<CartBloc>(
-          create: (context) => CartBloc(_cartRepository)..add(CartLoadEvent()),
+            create: (context) => sl<CartBloc>()..add(CartLoadEvent())),
+        BlocProvider<ProductBloc>(
+          create: (context) => sl<ProductBloc>()..add(ProductLoadEvent()),
         ),
-        BlocProvider(
-          create: (context) =>
-              ProductBloc(_onePhoneRepository)..add(ProductLoadEvent()),
-        ),
-        BlocProvider(
-          create: (context) =>
-              PhoneBloc(_phoneRepository)..add(LoadPhoneEvent()),
+        BlocProvider<HomeBloc>(
+          create: (context) => sl<HomeBloc>()..add(LoadHomeEvent()),
         )
       ],
       child: MaterialApp(
