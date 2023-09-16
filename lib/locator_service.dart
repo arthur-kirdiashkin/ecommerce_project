5da321764d75adeb9849edad9e2e48dd0e93b69a
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-16 10:00:46 +0300

    worked with clean arch for home,product,cart pages

--- lib/locator_service.dart
+++ lib/locator_service.dart
@@ -1,14 +1,47 @@
-import 'package:ecommerce_project/application/ui/home/domain/usecases/get_all_phones.dart';
-import 'package:ecommerce_project/application/ui/home/presentation/bloc/phone_bloc.dart';
+
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'features/cart/data/datasources/cart_remote_data_source.dart';
+import 'features/cart/data/repositories/cart_repository_impl.dart';
+import 'features/cart/domain/repositories/cart_repository.dart';
+import 'features/cart/domain/usecases/get_all_carts.dart';
+import 'features/cart/presentation/bloc/cart_bloc.dart';
+import 'features/home/data/datasources/home_remote_data_source.dart';
+import 'features/home/data/repositories/home_repository_impl.dart';
+import 'features/home/domain/repositories/home_repository.dart';
+import 'features/home/domain/usecases/get_all_home_store.dart';
+import 'features/home/presentation/bloc/home_bloc.dart';
+import 'features/product/data/datasources/product_remote_data_source.dart';
+import 'features/product/data/repositories/product_repository_impl.dart';
+import 'features/product/domain/repositories/product_repository.dart';
+import 'features/product/domain/usecases/get_all_products.dart';
+import 'features/product/presentation/bloc/product_bloc.dart';
 
 
-// // sl is ServiceLocator
-// final sl = GetIt.instance;
 
 
-// Future<void> init() async {
-//   // Bloc/ Cubit
-//   sl.registerFactory(() => PhoneBloc(getAllPhones: sl<GetAllPhones>()));
+final sl = GetIt.instance;
 
-// }
+Future<void> init() async {
+// BLoc/ Cubit
+  sl.registerFactory(() => HomeBloc(getAllHomeStore: sl<GetAllHomeStore>()));
+  sl.registerFactory(() => ProductBloc(getAllProducts: sl<GetAllProducts>()));
+  sl.registerFactory(() => CartBloc(getAllCarts: sl<GetAllCarts>()));
+
+// UseCases
+  sl.registerLazySingleton(() => GetAllHomeStore(sl()));
+  sl.registerLazySingleton(() => GetAllProducts(sl()));
+  sl.registerLazySingleton(() => GetAllCarts(sl()));
+
+// Repository
+sl.registerLazySingleton<HomeRepository>(() => HomeRepositoryImpl(homeRemoteDataSource: sl()));
+sl.registerLazySingleton<ProductRepository>(() => ProductRepositoryImpl(productRemoteDataSource: sl()));
+sl.registerLazySingleton<CartRepository>(() => CartRepositoryImpl( cartRemoteDataSource: sl()));
+
+sl.registerLazySingleton<HomeRemoteDataSource>(() => HomeRemoteDataSourceImpl(client: sl()));
+sl.registerLazySingleton<ProductRemoteDataSource>(() => ProductRemoteDataSourceImpl(client: sl()));
+sl.registerLazySingleton<CartRemoteDataSource>(() => CartRemoteDataSourceImpl(client: sl()));
+
+// External
+sl.registerLazySingleton(() => http.Client());
+}
