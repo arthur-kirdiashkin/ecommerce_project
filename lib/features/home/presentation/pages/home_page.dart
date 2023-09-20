c964842188eabe2cbe594ae26b0dfba5a782bafa
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-20 15:58:49 +0300

    Refactored Home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,31 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
-import '../../../../../locator_service.dart';
-import '../../data/datasources/home_remote_data_source.dart';
-import '../../data/models/phone_store_model.dart';
-import '../bloc/home_bloc.dart';
-import '../home_widgets/best_seller_example_provider.dart';
-import '../home_widgets/best_seller_widget.dart';
-import '../home_widgets/home_location_widget.dart';
-import '../home_widgets/hot_sales_widget.dart';
-import '../home_widgets/search_widget.dart';
-import '../home_widgets/select_category_widget.dart';
-import 'package:http/http.dart' as http;
+import '../home_widgets/BestSeller/best_sellers_widget.dart';
+import '../home_widgets/SelectCategory/home_location_widget.dart';
+import '../home_widgets/HotSales/hot_sales_widget.dart';
+import '../home_widgets/Search/search_widget.dart';
+import '../home_widgets/SelectCategory/select_category_widget.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // final _phoneRepository = PhoneRepository();
-
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  const Scaffold(
         body: SingleChildScrollView(
       child: Column(
         children: [
@@ -47,7 +31,7 @@ class _HomePageState extends State<HomePage> {
           Column(
             children: [
               HotSalesWidget(),
-              BestSellerWidget(),
+              BestSellersWidget(),
             ],
           )
         ],
@@ -55,3 +39,5 @@ class _HomePageState extends State<HomePage> {
     ));
   }
 }
+
+
