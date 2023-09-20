5c19aee83e41b035dd8c4819cbf034eaa675b412
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-20 16:11:03 +0300

    Refactored Home repository

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
-
-import '../../../core/error/failure.dart';
+import '../../../../core/error/failure.dart';
 import '../entities/best_seller_entity.dart';
 import '../entities/home_entiry.dart';
 import '../entities/phone_entity.dart';
