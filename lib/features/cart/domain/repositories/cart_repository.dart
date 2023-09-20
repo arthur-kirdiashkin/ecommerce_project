99653d6c0c3e629fc9ab9479356873634f88451c
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-20 18:43:44 +0300

    Refactored cart repository

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -1,16 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:ecommerce_project/application/ui/cart/domain/entities/basket_entity.dart';
-import 'package:ecommerce_project/application/ui/cart/domain/entities/cart_entity.dart';
-import '../../../core/error/failure.dart';
-
-// class CartRepository {
-//   ApiClientCart _apiClientCart = ApiClientCart();
-
-//   Future<List<CartEntity>> getAllCarts() => _apiClientCart.getCart();
-
-//   // Future<List<BasketEntity>> getAllCartBasket() => _apiClientCart.getBasket();
-
-// }
+import '../../../../core/error/failure.dart';
+import '../entities/basket_entity.dart';
+import '../entities/cart_entity.dart';
 
 abstract class CartRepository {
   Future<Either<Failure, List<CartEntity>>> getAllCarts();
