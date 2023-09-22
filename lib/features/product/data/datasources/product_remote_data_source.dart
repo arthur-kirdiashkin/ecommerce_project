1ba74cb2f0d0da4123adc1c44bbe400a705b5849
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-22 12:45:10 +0300

    Worked with product data source

--- lib/features/product/data/datasources/product_remote_data_source.dart
+++ lib/features/product/data/datasources/product_remote_data_source.dart
@@ -1,24 +1,7 @@
 import 'dart:convert';
-
-import 'package:ecommerce_project/application/ui/core/error/exception.dart';
-import 'package:ecommerce_project/application/ui/product/data/models/product_store_model.dart';
 import 'package:http/http.dart' as http;
-
-// class ApiClientProduct {
-//   Future<List<ProductModel>> getProduct() async {
-//     final urlProduct = Uri.parse(
-//         'https://run.mocky.io/v3/6c14c560-15c6-4248-b9d2-b4508df7d4f5');
-//     final response = await http.get(urlProduct);
-//     if (response.statusCode == 200) {
-//       final productJson = jsonDecode(response.body);
-//       List<ProductModel> list = [];
-//       ProductModel results = ProductModel.fromJson(productJson);
-//       list.add(results);
-//       return list;
-//     }
-//     throw Exception();
-//   }
-// }
+import '../../../../core/error/exception.dart';
+import '../models/product_store_model.dart';
 
 abstract class ProductRemoteDataSource {
   Future<List<ProductModel>> gellAllProducts();
