d8b15a36ac7f0a0985af87706f0dacfd35b8a716
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-22 12:46:59 +0300

    Refactored product repository

--- lib/features/product/domain/repositories/product_repository.dart
+++ lib/features/product/domain/repositories/product_repository.dart
@@ -1,18 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'package:ecommerce_project/application/ui/home/data/datasources/home_remote_data_source.dart';
-import 'package:ecommerce_project/application/ui/product/data/datasources/product_remote_data_source.dart';
-import 'package:ecommerce_project/application/ui/product/data/models/product_store_model.dart';
-import 'package:ecommerce_project/application/ui/product/domain/entities/product_entity.dart';
-
-import '../../../core/error/failure.dart';
-import '../../../home/domain/entities/home_entiry.dart';
-
-// class OnePhoneRepository {
-//   ApiClientProduct _apiClientProduct = ApiClientProduct();
-
-//   Future<List<ProductEntity>> getPhone() => _apiClientProduct.getProduct();
-
-// }
+import '../../../../core/error/failure.dart';
+import '../entities/product_entity.dart';
 
 abstract class ProductRepository {
   Future<Either<Failure, List<ProductEntity>>> gellAllProducts();
