c1d1cb8ac83f279240740842bc297099b392fd84
arthur-kirdiashkin <144155500+arthur-kirdiashkin@users.noreply.github.com>
2023-09-28 23:15:58 +0300

    Refactored product bloc

--- lib/features/product/presentation/bloc/product_event.dart
+++ lib/features/product/presentation/bloc/product_event.dart
@@ -1,8 +1,13 @@
 import 'package:equatable/equatable.dart';
 
-abstract class ProductEvent extends Equatable {}
+abstract class ProductEvent extends Equatable {
+  const ProductEvent();
+
+}
 
 class ProductLoadEvent extends ProductEvent {
+  const ProductLoadEvent();
+
   @override
   // TODO: implement props
   List<Object?> get props => [];
